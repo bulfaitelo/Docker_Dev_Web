8ce545f5ee8dbaba29573ef432987cc0417d8453
Thiago Rodrigues <bulfaitelo@gmail.com>
2023-03-08 11:48:39 -0300

    Implementando log de erros PHP

--- www/error.php
+++ www/error.php
@@ -1,2 +1,9 @@
 <?php
-wrreßß
+echo "teste";
+
+die("something bad happened!");
+
+trigger_error("something happened"); //error level is E_USER_NOTICE
+
+//You can control error level
+trigger_error("something bad happened", E_USER_ERROR);
