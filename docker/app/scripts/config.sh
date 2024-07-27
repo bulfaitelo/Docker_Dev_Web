9bdca24c8b34193408cdc82b7bf3b1eccaa8a7cc
Bulfaitelo <bulfaitelo@gmail.com>
2024-07-27 16:13:56 -0300

    Corrigindo docker-compose

--- docker/app/scripts/config.sh
+++ docker/app/scripts/config.sh
@@ -22,7 +22,11 @@ then
         # Apple Silicon
         elif [ "$APP_PLATFORM" = "arm64" ]
         then
-            echo "Apple Silicon: $APP_PLATFORM"        
+         
+            apt-get install -y gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils libgbm-dev libxshmfence-dev
+            npm install --location=global --unsafe-perm puppeteer@^17
+            chmod -R o+rx /usr/lib/node_modules/puppeteer/.local-chromium
+            runuser -l app -c 'npx --yes puppeteer browsers install chromium@1083080'     
         else
             echo "Unknown environment: $APP_PLATFORM"
             exit 1    
